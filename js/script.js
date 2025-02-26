fbd43a4840d718925a03ad56a0c91f8eff7b70c3
ThomasAlbt <thomas.albert.jv@gmail.com>
2025-02-26 21:15:56 +0100

    Added JS and updated nav

--- js/script.js
+++ js/script.js
@@ -2,8 +2,26 @@
 
 const burger = document.getElementById('burger');
 const nav = document.getElementById('nav');
+const navItems = document.querySelectorAll('#nav-visible ul li');
+const button = document.querySelector('button');
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('open');
     nav.classList.toggle('open');
+});
+
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.add('click-animate');
+        setTimeout(() => {
+            item.classList.remove('click-animate');
+        }, 300);
+    });
+});
+
+button.addEventListener('click', () => {
+    button.classList.add('click-animate');
+    setTimeout(() => {
+        button.classList.remove('click-animate');
+    }, 300);
 });
